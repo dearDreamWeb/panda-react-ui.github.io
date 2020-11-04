705a1d03122d8a7278ed4f2f8e05937c006b7bf5
wxb <superwang9527@163.com>
2020-11-04 09:16:44 +0800

    v1.0

---
+++ precache-manifest.1cbf8c35d434e5f3ab99111d8c15b52b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4034be54cb7c05d686fadff8f076c267",
+    "url": "./index.html"
+  },
+  {
+    "revision": "29af776229c8cdf20d6a",
+    "url": "./static/css/main.13c3f462.chunk.css"
+  },
+  {
+    "revision": "a83b99fc30af2c04612e",
+    "url": "./static/js/2.19302b73.chunk.js"
+  },
+  {
+    "revision": "e76ff9c56e9734d0311c8c16facfa3c7",
+    "url": "./static/js/2.19302b73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29af776229c8cdf20d6a",
+    "url": "./static/js/main.bde9c973.chunk.js"
+  },
+  {
+    "revision": "7b190492cd3e1e6f3175",
+    "url": "./static/js/runtime-main.948004f2.js"
+  },
+  {
+    "revision": "ec5bc1b3148ca464a3a3e17af89c3ead",
+    "url": "./static/media/github-brands.ec5bc1b3.svg"
+  },
+  {
+    "revision": "d51697f5f87ad53ea3a31f00c88ba925",
+    "url": "./static/media/logo.d51697f5.png"
+  }
+]);
